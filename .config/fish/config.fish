e823f55f7df5a5dda1fc299d3f36336c50a70468
Euan Jed Tabamo <euan.tabamo@gmail.com>
2026-04-28 20:21:14 +0800

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,32 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# PATHs & Environment Setup
+fish_add_path ~/.local/bin ~/bin
+fish_add_path ~/.deno/bin
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.vite-plus/bin
+
+# Environment Variables
+set -x EDITOR hx
+
+# Discord Bridge
+if not pgrep -f discord-ipc-bridge >/dev/null
+    ~/scripts/discord-ipc-bridge.sh &
+end
+
+# Completions
+
+COMPLETE=fish jj | source
+
+# Programs
+
+if type -q starship
+    starship init fish | source
+end
+
+# Zoxide
+if type -q zoxide
+    zoxide init fish | source
+end
